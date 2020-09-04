d5c0786ed57dc4daebeeb4c20f28da281ca46db2
Mary Claire Ahern <maryclaireahern@Marys-MacBook-Pro-2.local>
2020-09-04 12:39:36 -0500

    another change to the message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace examplerepo1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Git is great");
+            Console.WriteLine("Git is great..... It really is");
         }
     }
 }
